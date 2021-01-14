2f6755ebb5cec39729a6a308ebec99e3561a93bf
Maggie Summers <summers.maggie@gmail.com>
2021-01-14 13:47:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(
    baseURL: APP_URL,
    authorizationParams: {
      response_type: "code id_token",
-     audience: "https://expenses-api-new.vercel.app",
+     audience: "https://expenses-api",
      // 👇 add this 👇
      scope: "openid profile email read:reports",
      // 👆 add this 👆
